90072e28a1db6c51b161e6797a837438f3bd59e0
LittleCatBear <j.huguet9@gmail.com>
2023-06-03 15:19:11 -0400

    generate shoes and stores models, run migration

---
+++ test/channels/inventory_updates_channel_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class InventoryUpdatesChannelTest < ActionCable::Channel::TestCase
+  # test "subscribes" do
+  #   subscribe
+  #   assert subscription.confirmed?
+  # end
+end
