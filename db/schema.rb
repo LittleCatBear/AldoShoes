6b578232237e11efe7759191c302fa6f3d0d8478
LittleCatBear <j.huguet9@gmail.com>
2023-06-03 16:31:25 -0400

    Add and configure Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,24 +12,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_191718) do
+ActiveRecord::Schema[7.0].define(version: 20_230_603_191_718) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "shoes", force: :cascade do |t|
-    t.string "model"
-    t.integer "inventory"
-    t.bigint "store_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["store_id"], name: "index_shoes_on_store_id"
+  create_table 'shoes', force: :cascade do |t|
+    t.string 'model'
+    t.integer 'inventory'
+    t.bigint 'store_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['store_id'], name: 'index_shoes_on_store_id'
   end
 
-  create_table "stores", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'stores', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "shoes", "stores"
+  add_foreign_key 'shoes', 'stores'
 end
