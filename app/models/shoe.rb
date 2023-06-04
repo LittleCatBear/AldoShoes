7742fd360d2a3773588a2dcfc125bda8bdad2d70
LittleCatBear <j.huguet9@gmail.com>
2023-06-04 16:57:29 -0400

    Add basic model validations

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,4 +2,7 @@
 
 class Shoe < ApplicationRecord
   belongs_to :store
+
+  validates :model, presence: true, length: { maximum: 50 }, uniqueness: { scope: :store }
+  validates :inventory, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- test/models/shoe_test.rb
+++ test/models/shoe_test.rb
@@ -2,8 +2,56 @@
 
 require 'test_helper'
 
+# Unit tests file for Shoe model
 class ShoeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid? returns true if the shoe has valid attributes' do
+    shoe = Shoe.new(
+      model: 'Model1',
+      inventory: 3,
+      store: Store.create!(name: 'test store')
+    )
+
+    assert shoe.valid?
+  end
+
+  test 'valid? returns false if the model name is too long' do
+    shoe = Shoe.new(
+      model: 'a' * 51,
+      inventory: 3,
+      store: Store.create!(name: 'test store')
+    )
+    assert_not shoe.valid?
+  end
+
+  test 'valid? returns false if the inventory is negative' do
+    shoe = Shoe.new(
+      model: 'Model1',
+      inventory: -3,
+      store: Store.create!(name: 'test store')
+    )
+    assert_not shoe.valid?
+  end
+
+  test 'valid? returns true for the same model in 2 different stores' do
+    Shoe.create!(model: 'Model1', inventory: 3, store: Store.create!(name: 'test store'))
+
+    shoe2 = Shoe.new(
+      model: 'Model1',
+      inventory: 3,
+      store: Store.create!(name: 'test store')
+    )
+    assert shoe2.valid?
+  end
+
+  test 'valid? returns false for a duplicated model in the same stores' do
+    store = Store.create!(name: 'test store')
+    Shoe.create!(model: 'Model1', inventory: 3, store:)
+
+    shoe2 = Shoe.new(
+      model: 'Model1',
+      inventory: 3,
+      store:
+    )
+    assert_not shoe2.valid?
+  end
 end
